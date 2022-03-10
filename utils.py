8a381da31a9a214b7956735811ab8a2f8bd4763a
luigisaetta <luigi.saetta@gmail.com>
2022-03-10 09:13:20 +0100

    added optuna + cross validate

--- utils.py
+++ utils.py
@@ -20,68 +20,80 @@ from sklearn.metrics import make_scorer
 def add_features(df):
     # feature engineering
     df_new = df.copy()
-    
-    df_new['datetime'] = pd.to_datetime(df_new['datetime'])
-    
+
+    df_new["datetime"] = pd.to_datetime(df_new["datetime"])
+
     # this way I add 2 engineered features
-    df_new['hour'] = df_new['datetime'].dt.hour
-    df_new['year'] = df_new['datetime'].dt.year
-    
+    df_new["hour"] = df_new["datetime"].dt.hour
+    df_new["year"] = df_new["datetime"].dt.year
+
     return df_new
+
 
 #
 # define a custom scorer for ADSTuner
 # aligned with the scorer used in Kaggle leaderboard
 #
-def rmsle(y_pred, y_actual):
+def rmsle(y_actual, y_pred):
+    # 9/03 inverted order of params
+    # clip value at 0 to avoid nan
+    y_pred = np.where(y_pred >= 0, y_pred, 0)
+
     diff = np.log(y_pred + 1) - np.log(y_actual + 1)
     mean_error = np.square(diff).mean()
+
     return np.sqrt(mean_error)
+
 
 #
 # functions for categorical encoding
 #
 
 # first train label encoder
-TO_CODE = ['season', 'weather', 'year']
+TO_CODE = ["season", "weather", "year"]
+
 
 def train_encoders(df):
     le_list = []
 
     for col in TO_CODE:
-        print(f'train for coding: {col} ')
-    
+        print(f"train for coding: {col} ")
+
         le = LabelEncoder()
         le.fit(df[col])
-        
+
         le_list.append(le)
-    
+
     print()
-    
+
     return le_list
+
 
 # then use it
 def apply_encoders(df, le_list):
 
     for i, col in enumerate(TO_CODE):
-        print(f'Coding: {col} ')
-    
+        print(f"Coding: {col} ")
+
         le = le_list[i]
-    
+
         df[col] = le.transform(df[col])
-    
+
     # special treatment for windspeed
     # windpeed actually is integer badly rounded !!
     # print('Coding: windspeed')
     # df['windspeed'] = np.round(df['windspeed'].values).astype(int)
-    
+
     return df
 
+
 def show_tuner_results(tuner):
-    
+
     # to count completed
-    result_df = tuner.trials[tuner.trials['state'] == 'COMPLETE'].sort_values(by=['value'], ascending=False)
-    
+    result_df = tuner.trials[tuner.trials["state"] == "COMPLETE"].sort_values(
+        by=["value"], ascending=False
+    )
+
     print("ADSTuner session results:")
     print(f"ADSTuner has completed {result_df.shape[0]} trials")
     print()
@@ -89,7 +101,8 @@ def show_tuner_results(tuner):
     print(f"Parameters for the best trial are: {tuner.best_params}")
     print(f"The metric used to optimize is: {tuner.scoring_name}")
     print(f"The best score is: {round(tuner.best_score, 4)}")
-    
+
+
 def show_categoricals(df, thr):
     THR = thr
     FIGSIZE = (9, 6)
@@ -98,9 +111,7 @@ def show_categoricals(df, thr):
 
     # changed using list comprehension, to shorten code
     cols2 = [col for col in cols if df[col].nunique() < THR]
-    list_count2 = [
-        df[col].nunique() for col in cols if df[col].nunique() < THR
-    ]
+    list_count2 = [df[col].nunique() for col in cols if df[col].nunique() < THR]
 
     # plot
     plt.figure(figsize=FIGSIZE)
@@ -111,4 +122,3 @@ def show_categoricals(df, thr):
     plt.xticks(rotation=90)
     plt.ylabel("# of distinct values")
     plt.grid(True)
- 
